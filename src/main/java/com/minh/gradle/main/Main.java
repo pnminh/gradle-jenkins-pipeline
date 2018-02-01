3514eeb27ebecc49fd59493a63a9d8d6b8f72166
minhpham <minh.pham@dhigroupinc.com>
2018-02-01 09:07:05 -0600

    update automatic versioning

---
+++ src/main/java/com/minh/gradle/main/Main.java
@@ -0,0 +1,11 @@
+package com.minh.gradle.main;
+
+import org.joda.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+        System.out.println("Hello everyone");
+    }
+}
